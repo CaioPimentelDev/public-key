b8424cd232c7550e4a9d61957aa908bb0f148703
Caio <caiiopimentel@hotmail.com>
2024-06-08 07:42:56 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -20,12 +20,12 @@ export async function GET() {
                 throw error;
             }
         }
-        obterAccessToken()
+        const { accessToken, challenge } = await obterAccessToken()
 
         const data = {
             public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
             created_at: 1717674476733,
-            response: obterAccessToken()
+            response: accessToken + "//" + challenge
         };
 
         return NextResponse.json(data);
