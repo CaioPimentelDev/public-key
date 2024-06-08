23865e68c5b9c7dfcce650028172e842b1748945
Caio <caiiopimentel@hotmail.com>
2024-06-08 07:49:05 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -14,18 +14,17 @@ export async function GET() {
                     scope: 'certificate.create'
                 });
 
-                return response.data
+                console.log(response.data)
             } catch (error) {
                 console.error('Erro ao obter access token:', error);
                 throw error;
             }
         }
-        const { accessToken, challenge } = await obterAccessToken()
+        obterAccessToken()
 
         const data = {
             public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
             created_at: 1717674476733,
-            response: accessToken + "//" + challenge
         };
 
         return NextResponse.json(data);
