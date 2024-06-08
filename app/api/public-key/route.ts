f4c181c7cb7f415447c82554d5e7e4095d42a8d9
Caio <caiiopimentel@hotmail.com>
2024-06-08 06:38:38 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -1,17 +1,33 @@
+import axios from "axios";
 import { NextResponse } from "next/server"
 
-export async function GET(){
-    try{
+export async function GET() {
+    try {
 
 
         const data = {
             public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
             created_at: 1717674476733
-          };
-        
-          return NextResponse.json(data);
+        };
 
-    }catch(err){
-        return new NextResponse("internal error", {status: 500})
+        const obterAccessToken = async () => {
+            try {
+                const response = await axios.post('https://sandbox.api.pagseguro.com/oauth2/token', {
+                    grand_type: 'challenge',
+                    scope: 'certificate.create'
+                });
+
+                console.log(response.data)
+            } catch (error) {
+                console.error('Erro ao obter access token:', error);
+                throw error;
+            }
+        }
+        obterAccessToken()
+
+        return NextResponse.json(data);
+
+    } catch (err) {
+        return new NextResponse("internal error", { status: 500 })
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,8 @@
 "use client"
 
-import axios from "axios";
 import styles from "./page.module.css";
-import { useEffect } from "react";
 
 export default function Home() {
-
-  async function obterAccessToken() {
-    try {
-      const response = await axios.post('https://sandbox.api.pagseguro.com/oauth2/token', {
-        grand_type: 'challenge',
-        scope: 'certificate.create'
-      });
-  
-      console.log(response.data)
-    } catch (error) {
-      console.error('Erro ao obter access token:', error);
-      throw error;
-    }
-  }
-
-  useEffect(()=>{
-    obterAccessToken()
-  })
 
   return (
     <main className={styles.main}>
