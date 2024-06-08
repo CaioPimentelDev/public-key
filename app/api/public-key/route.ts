fa731ab6dc39c0dec4a2ff1ed2a7f4bde3c378c5
Caio <caiiopimentel@hotmail.com>
2024-06-08 08:01:34 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -7,20 +7,30 @@ export async function GET() {
 
         
 
-        const obterAccessToken = async () => {
+        const getTokenAndChallenge = async () => {
             try {
-                const response = await axios.post('https://sandbox.api.pagseguro.com/oauth2/token', {
-                    grand_type: 'challenge',
-                    scope: 'certificate.create'
-                });
-
-                console.log(response.data)
-            } catch (error) {
-                console.error('Erro ao obter access token:', error);
-                throw error;
-            }
-        }
-        obterAccessToken()
+            const response = await axios.post('https://sandbox.api.pagseguro.com/oauth2/token', {
+              grand_type: 'challenge',
+              scope: 'certificate.create'
+            });
+        
+            const { access_token, challenge } = response.data;
+        
+        
+            return { access_token, challenge };
+          } catch (error) {
+            console.error('Erro ao obter token de acesso e desafio:', error);
+            throw error;
+          }
+        };
+        
+        getTokenAndChallenge()
+          .then(({ access_token, challenge }) => {
+            console.log(access_token, challenge)
+          })
+          .catch(error => {
+            console.log(error+"não foi possivel fazer a requisição")
+          });
 
         const data = {
             public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
