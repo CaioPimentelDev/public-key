62f96a3ee02b7cb633a4d105a7711f7ad513b413
Caio <caiiopimentel@hotmail.com>
2024-06-10 08:59:17 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -4,8 +4,6 @@ import { NextResponse } from "next/server"
 export async function GET() {
   try {
 
-    const obterTokenEChallenge = async () => {
-
       const options = {
         method: 'POST',
         url: 'https://sandbox.api.pagseguro.com/oauth2/token',
@@ -24,10 +22,6 @@ export async function GET() {
         .catch(function (error: any) {
           console.error(error);
         });
-    }
-
-    // Chame a função para obter o token e o challenge quando necessário
-    obterTokenEChallenge();
 
     const data = {
       public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
