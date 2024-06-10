2fdaf4faf3a2f85b55254caccc16f47eb2386f1b
Caio <caiiopimentel@hotmail.com>
2024-06-10 10:37:44 -0300

    post

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
+import axios from "axios";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+
+  const test = await axios.get("/api/public-key")
+  console.log(test)
 
   return (
     <main className={styles.main}>
-      
+      <div>testPage</div>
     </main>
   );
 }
