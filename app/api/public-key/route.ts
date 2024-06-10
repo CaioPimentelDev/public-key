02a85dcde646fc98e94c41c1304ea7647b3fca6a
Caio <caiiopimentel@hotmail.com>
2024-06-10 09:21:31 -0300

    post

--- app/api/public-key/route.ts
+++ app/api/public-key/route.ts
@@ -16,15 +16,12 @@ export async function GET() {
         data: { grant_type: 'challenge' }
       };
 
-      axios.request(options)
-        .then(function (response: { data: any; }) {
-          console.log(response.data);
-        })
+      const response = await axios.request(options)
         .catch(function (error: any) {
           console.error(error);
         });
 
-        console.log("test")
+        console.log(response)
 
     const data = {
       public_key: "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxEdKLEk2xK/k7X1gfbGH+K4bPF7TC+1g8DpzEnYQzEOWEnmFwibhzl6UwTy4ItyOKUWEeLfjpCUHSzZ+vPMDjrb8VKj6RbJOwSMjM83A8rgPpvNWmsuq/ZLskBat+qWZiV+JiCSs3SFbMyrzPQKEolZgHQOeUlM7RTIVZJnwAqaWO4/v1+FVm4/2rLXNAr5oL/8XczGG0swURTcVdriwUK3XW6eIXQe74lC/MxJmNdxnWFaKaQCbCrnsoWAj7CHBwa+u8QnhFoX0JljWYFOH/B1mGzoVlzZMRi1X9NmhgUNiy3Ust/BZJVaETaDE2SWCsGMqjePH0COMOLdWNNDU+QIDAQAB-----END PUBLIC KEY-----",
